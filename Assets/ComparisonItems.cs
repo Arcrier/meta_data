a9a24ca8faa07501d853015bf2d4f289dc1d7f24
Arcrier <adhardy13@gmail.com>
2022-04-09 09:25:18 -0600

    Added return sphere, set up scripts for teleportation and item spawning - State as it was during class presentation

--- Assets/ComparisonItems.cs
+++ Assets/ComparisonItems.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using Unity.XR.CoreUtils;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
 
 public class ComparisonItems : MonoBehaviour
 {
@@ -33,7 +37,8 @@ public class ComparisonItems : MonoBehaviour
 
     void Start()
     {
-        csv = getCSVData();
+        //csv = getCSVData();
+        //setUpComparisons(donut);
     }
 
 
@@ -44,17 +49,108 @@ public class ComparisonItems : MonoBehaviour
 
     private string[][] getCSVData()
     {
-
-        return null;
+        string[][] priceArray = File.ReadLines("Assets/FoodPrice.csv").Select(line => Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)")).ToArray();
+        return priceArray;
     }
 
     public void setUpComparisons(GameObject obj)
     {
+        List<GameObject> childList = new List<GameObject>();
+        for(int i = 0; i < transform.childCount; i ++) {
+            childList.Add(transform.GetChild(i).gameObject);
+        }
+        foreach(GameObject child in childList) {
+            Destroy(child);
+        }
         for (int i = 0; i < 5; i++)
         {
             GameObject comparisonObject = Instantiate(prefab, transform);
             comparisonObject.transform.position -= new Vector3(0, 0, 0.75f);
             comparisonObject.transform.position += new Vector3(0, 0.15f, 0.375f * i);
+            switch(i)
+            {
+                case 0:
+                    if (obj.name == "Donuts")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Aldi";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$11.88";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "12 Donuts";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$0.99 / 1 Donut";
+                    }
+                    else if (obj.name == "HamEgg")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Aldi";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$12.49";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "8";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$1.56 / Sandwich";
+                    }
+                    break;
+                case 1:
+                    if (obj.name == "Donuts")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Kroger";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$10.68";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "12 Donuts";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$0.89 / 1 Donut";
+                    }
+                    else if (obj.name == "HamEgg")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Kroger";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$5.99";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "4";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$1.49 / Sandwich";
+                    }
+                    break;
+                case 2:
+                    if (obj.name == "Donuts")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Trader Joe's";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$15.48";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "12 Donuts";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$1.29 / 1 Donut";
+                    }
+                    else if (obj.name == "HamEgg")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Trader Joe's";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$11.99";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "6";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$2.00 / Sandwich";
+                    }
+                    break;
+                case 3:
+                    if (obj.name == "Donuts")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Publix";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$11.88";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "12 Donuts";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$0.99 / 1 Donut";
+                    }
+                    else if (obj.name == "HamEgg")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Publix";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$12.49";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "8";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$1.56 / Sandwich";
+                    }
+                    break;
+                case 4:
+                    if (obj.name == "Donuts")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Walmart";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$9.48";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "12 Donuts";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$0.79 / 1 Donut";
+                    }
+                    else if (obj.name == "HamEgg")
+                    {
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("NameText").GetComponent<Text>().text = "Walmart";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceText").GetComponent<Text>().text = "$11.99";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("AmountText").GetComponent<Text>().text = "8";
+                        comparisonObject.GetNamedChild("Canvas").GetNamedChild("PriceAmountText").GetComponent<Text>().text = "$1.49 / Sandwich";
+                    }
+                    break;
+            }
+            
             GameObject comparisonSubObject = Instantiate(obj, comparisonObject.transform);
             comparisonSubObject.transform.localPosition = new Vector3(0, -0.15f, 0);
             XRSimpleInteractable interactable = comparisonSubObject.GetComponent<XRSimpleInteractable>();
--- Assets/SceneTransitionManager.cs
+++ Assets/SceneTransitionManager.cs
@@ -6,7 +6,9 @@ public class SceneTransitionManager : MonoBehaviour
 {
     public FadeScreen fadeScreen;
     //private Object player;
-    public Vector3 destination;
+    public Vector3 destinationRoom;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
     public Quaternion rotation;
     private CharacterController character;
 
@@ -22,13 +24,15 @@ public class SceneTransitionManager : MonoBehaviour
 
     public void GoToScene()
     {
-        IEnumerator coroutine = GoToSceneRoutine(destination);
+        IEnumerator coroutine = GoToSceneRoutine(destinationRoom);
         StartCoroutine(coroutine);
     }
 
     IEnumerator GoToSceneRoutine(Vector3 destination)
     {
         otherRoom = true;
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
         fadeScreen.FadeOut();
         yield return new WaitForSeconds(fadeScreen.fadeDuration);
 
@@ -41,5 +45,29 @@ public class SceneTransitionManager : MonoBehaviour
 
     }
 
-    
+    public void Return()
+    {
+        IEnumerator coroutine = Return(originalPosition);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator Return(Vector3 destination)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+
+        //Launch the new scene
+        character.enabled = false;
+        transform.SetPositionAndRotation(destination, originalRotation);
+        character.enabled = true;
+        Debug.Log(destination);
+        Debug.Log(transform.position);
+
+        otherRoom = false;
+
+        fadeScreen.FadeIn();
+
+    }
+
+
 }
